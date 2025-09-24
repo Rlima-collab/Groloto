da4ddb44d547b62887157483a6bf4df09d638ec2
LIMA Romain <romain.lima1@etu.univ-orleans.fr>
2025-09-24 17:10:43 +0200

    conversion entité Utilisateur vers attributs PHP 8 + UserInterface
    - Conversion des annotations @ORM vers les attributs #[ORM]
    - Implémentation de UserInterface et PasswordAuthenticatedUserInterface
    - Ajout des méthodes d'authentification (getRoles, getPassword, etc.)
    - Ajout des contraintes de validation avec attributs
    - Correction du type de retour eraseCredentials(): void

--- GroLoto/src/Entity/Utilisateur.php
+++ GroLoto/src/Entity/Utilisateur.php
@@ -2,42 +2,192 @@
 // src/Entity/Utilisateur.php
 namespace App\Entity;
 
+use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="UTILISATEUR")
- */
-class Utilisateur
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[ORM\Table(name: "UTILISATEUR")]
+#[UniqueEntity(fields: ["email"], message: "Cette adresse email est déjà utilisée")]
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /** @ORM\Id @ORM\GeneratedValue @ORM\Column(type="integer") */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
     private $id;
 
-    /** @ORM\ManyToOne(targetEntity="Role") 
-     *  @ORM\JoinColumn(name="id_role", referencedColumnName="id", nullable=false)
-     */
+    #[ORM\ManyToOne(targetEntity: "Role")]
+    #[ORM\JoinColumn(name: "id_role", referencedColumnName: "id", nullable: false)]
     private $role;
 
-    /** @ORM\Column(type="string", unique=true) */
+    #[ORM\Column(type: "string", length: 180, unique: true)]
+    #[Assert\NotBlank(message: "L'email est obligatoire")]
+    #[Assert\Email(message: "L'email n'est pas valide")]
     private $email;
 
-    /** @ORM\Column(type="string", nullable=true) */
+    #[ORM\Column(type: "string", nullable: true)]
     private $mot_de_passe;
 
-    /** @ORM\Column(type="string", nullable=true) */
+    #[ORM\Column(type: "string", nullable: true)]
     private $prenom;
 
-    /** @ORM\Column(type="string", nullable=true) */
+    #[ORM\Column(type: "string", nullable: true)]
     private $nom;
 
-    /** @ORM\Column(type="string", nullable=true) */
+    #[ORM\Column(type: "string", nullable: true)]
     private $telephone;
 
-    /** @ORM\Column(type="datetime", nullable=true) */
+    #[ORM\Column(type: "datetime", nullable: true)]
     private $date_creation;
 
-    /** @ORM\Column(type="datetime", nullable=true) */
+    #[ORM\Column(type: "datetime", nullable: true)]
     private $date_modification;
 
-    // getters & setters
+    // UserInterface methods
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = [];
+        if ($this->role) {
+            switch ($this->role->getNom()) {
+                case 'admin':
+                    $roles[] = 'ROLE_ADMIN';
+                    break;
+                case 'benevole':
+                    $roles[] = 'ROLE_BENEVOLE';
+                    break;
+                case 'mecene':
+                    $roles[] = 'ROLE_MECENE';
+                    break;
+                default:
+                    $roles[] = 'ROLE_USER';
+            }
+        }
+        
+        // Garantir que chaque utilisateur a au moins ROLE_USER
+        $roles[] = 'ROLE_USER';
+        
+        return array_unique($roles);
+    }
+
+    public function getPassword(): string
+    {
+        return $this->mot_de_passe;
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // Si vous stockez des données sensibles temporaires sur l'utilisateur, effacez-les ici
+    }
+
+    // Getters & Setters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->mot_de_passe;
+    }
+
+    public function setMotDePasse(string $mot_de_passe): self
+    {
+        $this->mot_de_passe = $mot_de_passe;
+        return $this;
+    }
+
+    public function getRole(): ?Role
+    {
+        return $this->role;
+    }
+
+    public function setRole(?Role $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): self
+    {
+        $this->telephone = $telephone;
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(?\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+        return $this;
+    }
+
+    public function getDateModification(): ?\DateTimeInterface
+    {
+        return $this->date_modification;
+    }
+
+    public function setDateModification(?\DateTimeInterface $date_modification): self
+    {
+        $this->date_modification = $date_modification;
+        return $this;
+    }
 }
