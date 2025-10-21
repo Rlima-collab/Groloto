b37e3642b935204b0846cbde35d1678b4b19ead9
AlexyWCK <you@example.com>
2025-10-21 12:04:17 +0200

    correction

--- GroLoto/src/Controller/EvenementController.php
+++ GroLoto/src/Controller/EvenementController.php
@@ -69,7 +69,7 @@ class EvenementController extends AbstractController
                     'evenement' => $creneau->getEvenement() ? $creneau->getEvenement()->getNom() : null,
                     'poste_requis' => $creneau->getPosteRequis(),
                     'max_personnes' => $creneau->getMaxPersonnes(),
-                    'notes' => $creneau->getNotes(),
+                    'remarques' => $creneau->getRemarques(),
                     'titre' => $creneau->getTitre()
                 ]
             ];
