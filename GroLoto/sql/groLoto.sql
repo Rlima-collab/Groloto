7ded29b367e41cc07a15ffd61016b5f9c7d9a865
AlexyWCK <you@example.com>
2025-11-07 11:03:13 +0100

    modif tache notif

--- GroLoto/sql/groLoto.sql
+++ GroLoto/sql/groLoto.sql
@@ -414,7 +414,6 @@ INSERT INTO AFFECTATION_TACHE (id_tache, id_benevole, id_utilisateur, statut, re
   (4, 2, 1, 'assigne', 'Affecté à l''accueil billetterie (benevole2)');
 
 INSERT INTO DEMANDE_TACHE (id_tache, id_benevole, statut, message_benevole, date_demande) VALUES
-  (2, 1, 'en_attente', 'Je souhaite participer au service bar', '2024-11-10 10:00:00'),
   (3, 2, 'acceptee', 'Disponible pour le montage', '2025-11-01 14:00:00'),
   (4, 1, 'refusee', 'Intéressé par l''accueil', '2025-11-02 09:00:00');
 
