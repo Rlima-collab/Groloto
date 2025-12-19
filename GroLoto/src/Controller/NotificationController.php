67056e017df5bb81d7741a7479125de3a7d9e972
AlexyWCK <you@example.com>
2025-12-19 14:05:46 +0100

    notifs

--- GroLoto/src/Controller/NotificationController.php
+++ GroLoto/src/Controller/NotificationController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -14,12 +15,26 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class NotificationController extends AbstractController
 {
     #[Route('/notifications', name: 'app_notifications')]
-    public function index(NotificationRepository $notificationRepo): Response
+    public function index(NotificationRepository $notificationRepo, Request $request): Response
     {
         $user = $this->getUser();
-        $notifications = $notificationRepo->findByUser($user);
+        $view = $request->query->get('view', 'inbox');
+
+        if ($view === 'trash') {
+            $notifications = $notificationRepo->createQueryBuilder('n')
+                ->where('n.destinataire = :user')
+                ->andWhere('n.type = :type')
+                ->setParameter('user', $user)
+                ->setParameter('type', 'corbeille')
+                ->orderBy('n.createdAt', 'DESC')
+                ->getQuery()
+                ->getResult();
+        } else {
+            $notifications = $notificationRepo->findByUser($user);
+        }
         return $this->render('notification/index.html.twig', [
             'notifications' => $notifications,
+            'view' => $view,
         ]);
     }
 
@@ -65,6 +80,28 @@ class NotificationController extends AbstractController
     {
         $user = $this->getUser();
         $notificationRepo->markAllAsReadByUser($user);
+        $em->flush();
+        return new JsonResponse(['success' => true]);
+    }
+
+    #[Route('/notifications/{id}/corbeille', name: 'app_notification_trash', methods: ['POST'])]
+    public function trash(Notification $notification, EntityManagerInterface $em): JsonResponse
+    {
+        if ($notification->getDestinataire() !== $this->getUser()) {
+            return new JsonResponse(['error' => 'Accès refusé'], 403);
+        }
+        $notification->setType('corbeille');
+        $em->flush();
+        return new JsonResponse(['success' => true]);
+    }
+
+    #[Route('/notifications/{id}/supprimer', name: 'app_notification_delete', methods: ['POST'])]
+    public function delete(Notification $notification, EntityManagerInterface $em): JsonResponse
+    {
+        if ($notification->getDestinataire() !== $this->getUser()) {
+            return new JsonResponse(['error' => 'Accès refusé'], 403);
+        }
+        $em->remove($notification);
         $em->flush();
         return new JsonResponse(['success' => true]);
     }
